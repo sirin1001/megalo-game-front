bc718ab804cb94c0c5621f39ea5c2ecaed8e8885
KatoDaikiaster <aou0711z@gmail.com>
2022-12-14 17:06:06 +0900

    Move作りました

--- megalo-game-front/Assets/Scripts/Player.cs
+++ megalo-game-front/Assets/Scripts/Player.cs
@@ -13,6 +13,44 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Move();
+    
+
+    }
+
+    void Move()
+    {
+        //âE
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            this.transform.Translate(10, 0, 0);
+        }
+        //ç∂
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            this.transform.Translate(-10, 0, 0);
+        }
+        //è„
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            this.transform.Translate(0, 10, 0);
+        }
+        //â∫
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            this.transform.Translate(0, -10, 0);
+        }
+        
         
     }
+
+
+    void Shot()
+    {
+        
+        
+
+    }
+
 }
+
